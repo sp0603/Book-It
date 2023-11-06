a840926e1d326b4cd4923c5601e93bca092450ef
De'Onna Brammer <ukdeonna@gmail.com>
2023-11-06 16:35:41 -0500

    added base tablayout, contactspage, default fragments for other tabs, altered MainActivity to switch to home screen once user is logged in

---
+++ app/src/main/java/com/example/scheduling/ContactsPage.java
@@ -0,0 +1,76 @@
+package com.example.scheduling;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class ContactsPage extends Fragment {
+
+    ListView listView;
+    ArrayList<User> userArrayList;
+    private ListItemAdapter adapter;
+    public ContactsPage() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_contacts_page, container, false);
+
+        //1- AdapterView: a ListView
+        listView = view.findViewById(R.id.listview);
+
+        //2- Data Source: ArrayList<User>
+        userArrayList = new ArrayList<>();
+
+        User user1 = new User("Joe Smith", "sample text", R.drawable.profileimg);
+        User user2 = new User("Kara Adams", "sample text", R.drawable.profileimg);
+        User user3 = new User("Jessica Lee", "sample text", R.drawable.profileimg);
+        User user4 = new User("Hailey Cardenas", "sample text", R.drawable.profileimg);
+        User user5 = new User("Andrew Ortega", "sample text", R.drawable.profileimg);
+        User user6 = new User("Victor Jarvis", "sample text", R.drawable.profileimg);
+        User user7 = new User("Matthew Beck", "sample text", R.drawable.profileimg);
+        User user8 = new User("Mason Green", "sample text", R.drawable.profileimg);
+        User user9 = new User("Breanna Fox", "sample text", R.drawable.profileimg);
+
+
+        userArrayList.add(user1);
+        userArrayList.add(user2);
+        userArrayList.add(user3);
+        userArrayList.add(user4);
+        userArrayList.add(user5);
+        userArrayList.add(user6);
+        userArrayList.add(user7);
+        userArrayList.add(user8);
+        userArrayList.add(user9);
+
+        //Adapter:
+        adapter = new ListItemAdapter(userArrayList, requireContext());
+        listView.setAdapter(adapter);
+
+        //handling click events
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // toast message will pop up upon clicking on a list item
+                // will change later to open up that user's profile?
+                Toast.makeText(requireContext(), "TEST", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        ///
+        return view;
+    }
+}
--- app/src/main/java/com/example/scheduling/MainActivity.java
+++ app/src/main/java/com/example/scheduling/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.scheduling;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import androidx.annotation.NonNull;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +12,18 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
     Button button;
     TextView textView;
     FirebaseUser user;
+    ViewPager2 viewPager;
+    ViewPagerAdapter myAdapter;
+    TabLayout tabLayout;
+    String[] tabNames = {"Home", "New Event", "Contacts", "Profile"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
+        //button = findViewById(R.id.logout);
+        //textView = findViewById(R.id.user_details);
         user = auth.getCurrentUser();
         if(user == null){
             Intent intent = new Intent(getApplicationContext(), Login.class);
@@ -33,17 +41,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
         else {
-            textView.setText(user.getEmail());
+            setContentView(R.layout.tablayout);
+            tabLayout = findViewById(R.id.tablayout);
+
+            myAdapter = new ViewPagerAdapter(
+                    getSupportFragmentManager(),
+                    getLifecycle()
+            );
+
+            // adding fragments to the list in the Adapter class
+            myAdapter.addFragment(new HomeFragment());
+            myAdapter.addFragment(new CreateEvent());
+            myAdapter.addFragment(new ContactsPage());
+            myAdapter.addFragment(new ProfileFragment());
+
+            // set the orientation in ViewPager2
+            viewPager = findViewById(R.id.viewPager2);
+            viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+
+            // connecting the adapter with the ViewPager2
+            viewPager.setAdapter(myAdapter);
+
+            // connecting TabLayout with ViewPager
+            new TabLayoutMediator(
+                    tabLayout,
+                    viewPager,
+                    new TabLayoutMediator.TabConfigurationStrategy() {
+                        @Override
+                        public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                            tab.setText(tabNames[position]);
+                        }
+                    }
+            ).attach();
         }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        // commented this out b/c I don't think it's being used currently
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view){
+//                FirebaseAuth.getInstance().signOut();
+//                Intent intent = new Intent(getApplicationContext(), Login.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
     }
 }
