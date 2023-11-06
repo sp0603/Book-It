135d04e4f4efb968f0b69f28f7cf3f2312a3b7bb
De'Onna Brammer <ukdeonna@gmail.com>
2023-11-06 16:39:11 -0500

    added viewpager2 dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 }
