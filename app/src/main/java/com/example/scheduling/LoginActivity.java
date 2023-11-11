72f32513127876df6614b649e90287ea76153b40
sp603 <syd.porter03@gmail.com>
2023-11-11 12:01:04 -0500

    changed log in.xml to match register

--- app/src/main/java/com/example/scheduling/LoginActivity.java
+++ app/src/main/java/com/example/scheduling/LoginActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,7 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-    TextInputEditText editTextEmail, editTextPassword;
+    EditText editTextEmail, editTextPassword;
     Button buttonLogin;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
