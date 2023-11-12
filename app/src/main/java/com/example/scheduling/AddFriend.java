10f3366d891b72c51ac539912a56b75ac6997eab
De'Onna Brammer <ukdeonna@gmail.com>
2023-11-12 03:20:51 -0500

    added functionality of adding friends in database, not reflected on contact page yet

--- app/src/main/java/com/example/scheduling/AddFriend.java
+++ app/src/main/java/com/example/scheduling/AddFriend.java
@@ -3,12 +3,21 @@ package com.example.scheduling;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddFriend extends AppCompatActivity {
     TextInputLayout textInputLayout;
@@ -22,6 +31,7 @@ public class AddFriend extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.add_friend);
 
         textInputLayout = findViewById(R.id.addFriendEditText);
@@ -39,8 +49,38 @@ public class AddFriend extends AppCompatActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = emailEditText.getText().toString();
-                // search for email in db, and if found add to user's friends
+                String email = emailEditText.getText().toString().trim();
+
+                // search for email in db
+                DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users");
+                DatabaseReference friendsRef = FirebaseDatabase.getInstance().getReference("friends");
+                usersRef.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            //email is registered in database
+                            for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                                String userId = userSnapshot.getKey();
+                                String userName = userSnapshot.child("name").getValue(String.class);
+                                //add to friends of current user
+                                String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                                friendsRef.child(currentUserId).child(userId).setValue(userName);
+
+                                Toast.makeText(AddFriend.this, "Friend added", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+                        else {
+                            //email not registered in database
+                            Toast.makeText(AddFriend.this, "Email Not Found", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(AddFriend.this, "ERROR", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
