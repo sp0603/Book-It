be3cf8d135e4148985a43164f3486121a32e6f51
sp603 <syd.porter03@gmail.com>
2023-11-15 09:45:45 -0500

    added the settings activity

--- app/src/main/java/com/example/scheduling/ProfileFragment.java
+++ app/src/main/java/com/example/scheduling/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.scheduling;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
+    ImageButton profilebutton;
+    ImageButton settingbutton;
+    TextView usernameText;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -18,6 +24,24 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        profilebutton = view.findViewById(R.id.profileButton);
+        settingbutton = view.findViewById(R.id.settingButton);
+        usernameText = view.findViewById(R.id.usernameDisplay);
+
+//        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        settingbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(requireContext(), Settings.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+
     }
 }
