5a5301641c38188b03c9dac23db587ab1e4ac688
sp603 <syd.porter03@gmail.com>
2023-12-03 02:44:20 -0500

    adding comments for clarification

--- app/src/main/java/com/example/scheduling/CreateEvent.java
+++ app/src/main/java/com/example/scheduling/CreateEvent.java
@@ -4,20 +4,33 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.nio.channels.Selector;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 public class CreateEvent extends Fragment {
@@ -32,7 +45,7 @@ public class CreateEvent extends Fragment {
     TextView selectedDateTextView;
     Button selectDateButton;
 
-    Button addFriendButton;
+    Spinner addFriendSpinner;
 
     public CreateEvent() {
         // Required empty public constructor
@@ -74,14 +87,8 @@ public class CreateEvent extends Fragment {
             }
         });
 
-        addFriendButton = view.findViewById(R.id.addFriendEvent);
-
-        addFriendButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                // fill in the gaps
-            }
-        });
+        //add friend to event
+        //not yet completed
 
         return view;
     }
